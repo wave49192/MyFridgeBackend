35ad71ffde2734db2d19f0aba0e6b4b30d7ab40f
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-03-19 14:42:35 +0700

    add 2 fields to model

--- RecommendationSystem/models.py
+++ RecommendationSystem/models.py
@@ -11,6 +11,8 @@ class Recipe(models.Model):
     source_url = models.URLField()
     cooking_time = models.IntegerField()
     ingredients = models.TextField()
+    cuisine_type = models.CharField(max_length=255)
+    cleaned_ingredients = models.TextField()
 
     def __str__(self):
         return self.title
