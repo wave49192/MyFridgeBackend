abde41ea87bb67d452b3f01c789cb0081e2fe6f2
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-03-23 22:20:11 +0700

    remove the current code because it is not working correctly

--- RecommendationSystem/management/commands/find_similar_recipe_2.py
+++ RecommendationSystem/management/commands/find_similar_recipe_2.py
@@ -6,64 +6,17 @@ import torch.nn.functional as F
 class RecommendationSystem:
     @staticmethod
     def find_similar_recipes_by_ingredients(favorite_recipe, recipes):
-        favorite_ingredients = favorite_recipe['cleaned_ingredients'].split(', ')
-        recipe_ingredients = [recipe['cleaned_ingredients'].split(', ') for recipe in recipes]
+        ingredient_union = lambda favorite_recipes: set().union(*[set(recipe['cleaned_ingredients'].split(', ')) for recipe in favorite_recipes])
+        favorite_ingredients = ingredient_union(favorite_recipe) #list of all ingredient that are in the favourite recipe
+        recipe_ingredients = [recipe['cleaned_ingredients'].split(', ') for recipe in recipes] #a specific recipe that i want to compare with.
         
-        favorite_embedding = torch.zeros(len(favorite_ingredients))
-        for idx, ingredient in enumerate(favorite_ingredients):
-            favorite_embedding[idx] = recipe_ingredients[0].index(ingredient) if ingredient in recipe_ingredients[0] else 0
+        #
+       #todo fill in the code by finding similarity
+     
+
         
-        recipe_embeddings = torch.zeros(len(recipes), len(favorite_embedding))
-        for i, recipe in enumerate(recipe_ingredients):
-            for idx, ingredient in enumerate(favorite_ingredients):
-                recipe_embeddings[i][idx] = recipe.index(ingredient) if ingredient in recipe else 0
-        
-        similarities = F.cosine_similarity(favorite_embedding.unsqueeze(0), recipe_embeddings)
-        
-        sorted_indices = torch.argsort(similarities, descending=True)
-        
-        favorite_recipe_index = recipes.index(favorite_recipe)
-        sorted_indices = sorted_indices[sorted_indices != favorite_recipe_index]
-        
-        top_10_similar_recipes = [(recipes[i]['title'], similarities[i].item()) for i in sorted_indices[:10]]
+        top_10_similar_recipes = []
         return top_10_similar_recipes
-
-    # @staticmethod
-
-    # def find_similar_recipes_by_cuisine(favorite_recipes, recipes):
-    #     print(favorite_recipes)
-    #     combined_cuisines = [favorite_recipe['cuisine_type'] for favorite_recipe in favorite_recipes]
-    #     combined_cuisines = list(set(combined_cuisines))
-        
-    #     # Initialize combined embedding tensor with the correct size
-    #     combined_embedding = torch.zeros(len(favorite_recipes), len(recipes[0]['cuisine_type']))
-        
-    #     # Create a mapping from cuisine type to index in the embeddings
-    #     cuisine_to_index = {cuisine: i for i, cuisine in enumerate(recipes[0]['cuisine_type'])}
-        
-    #     for i, favorite_recipe in enumerate(favorite_recipes):
-    #         favorite_cuisine = favorite_recipe['cuisine_type']
-    #         favorite_index = cuisine_to_index.get(favorite_cuisine)
-    #         if favorite_index is not None:
-    #             combined_embedding[i][favorite_index] = 1
-        
-    #     recipe_embeddings = torch.zeros(len(recipes), len(recipes[0]['cuisine_type']))
-    #     for i, recipe in enumerate(recipes):
-    #         recipe_cuisine = recipe['cuisine_type']
-    #         for j, cuisine in enumerate(recipe_cuisine):
-    #             recipe_embeddings[i][j] = 1
-        
-    #     # Calculate cosine similarity for each favorite recipe
-    #     similarities = F.cosine_similarity(combined_embedding.unsqueeze(1), recipe_embeddings, dim=2)
-        
-    #     # Combine similarities for all favorite recipes
-    #     combined_similarities = torch.mean(similarities, dim=0)
-        
-    #     # Sort recipes based on combined similarity
-    #     sorted_indices = torch.argsort(combined_similarities, descending=True)
-        
-    #     top_10_similar_recipes = [(recipes[i]['title'], combined_similarities[i].item()) for i in sorted_indices[:10]]
-    #     return top_10_similar_recipes
 
 
     @staticmethod
@@ -85,30 +38,39 @@ class RecommendationSystem:
         ]
         
         similar_recipes_by_ingredients = RecommendationSystem.find_similar_recipes_by_ingredients(favorite_recipe, recipes)
-        print("Similar recipes by ingredients:")
-        for recipe, similarity in similar_recipes_by_ingredients:
-            print(f"{recipe}: {similarity}")
         
-        # similar_recipes_by_cuisine = RecommendationSystem.find_similar_recipes_by_cuisine(favorite_recipe, recipes)
-        # print("\nSimilar recipes by cuisine:")
-        # for recipe, similarity in similar_recipes_by_cuisine:
-        #     print(f"{recipe}: {similarity}")
+        similar_recipes_score = {}
+        print("Similar recipes by ingredients:")
+        ## fill in this code to store all the similarity score of every recipe comparing to the favourite recipe.
+        
+
 
 class Command(BaseCommand):
     help = 'Recommend similar recipes based on a favorite recipe'
 
     def handle(self, *args, **kwargs):
-        favorite_recipe = {
-            "recipe_id": "5ed6604591c37cdc054bc9ec",
-            "title": "1-minute berry ice cream",
-            "image_url": "http://forkify-api.herokuapp.com/images/2_1_1350376582_lrg6103.jpg",
-            "publisher": "Jamie Oliver",
-            "source_url": "http://www.jamieoliver.com/recipes/fruit-recipes/1-minute-berry-ice-cream",
-            "cooking_time": 45,
-            "ingredients": "500 g mixed frozen berries, 150 g fresh blueberries, 7 tbsps runny honey, 500 g natural yoghurt, None  A few sprigs fresh mint, 4  small ice cream cornets optional",
-            "cuisine_type": "Mexican",
-            "cleaned_ingredients": "mixed frozen berries, fresh blueberries, runny honey, natural yoghurt, None  A few sprigs fresh mint, small ice cream cornets optional"
-        }
+        favorite_recipe =     [{
+        "recipe_id": "5ed6604591c37cdc054bcac4",
+        "title": "Pizza Dip",
+        "image_url": "http://forkify-api.herokuapp.com/images/Pizza2BDip2B12B500c4c0a26c.jpg",
+        "publisher": "Closet Cooking",
+        "source_url": "http://www.closetcooking.com/2011/03/pizza-dip.html",
+        "cooking_time": 60,
+        "ingredients": "4 oz cream cheese room temperature, 0.25 cup sour cream, 0.25 cup mayonnaise, 0.5 cup mozzarella grated, 0.25 cup parmigiano reggiano grated, 1 cup pizza sauce, 0.5 cup mozzarella shredded/grated, 0.25 cup parmigiano reggiano grated, 2 oz pepperoni sliced, 2 tbsps green pepper sliced, 2 tbsps black olives sliced",
+        "cuisine_type": "German",
+        "cleaned_ingredients": "cream cheese room temperature, sour cream, mayonnaise, mozzarella grated, parmigiano reggiano grated, pizza sauce, mozzarella shredded/grated, parmigiano reggiano grated, pepperoni sliced, green pepper sliced, black olives sliced"
+    },    {
+        "recipe_id": "5ed6604591c37cdc054bcd08",
+        "title": "Pomegranate Yogurt Bowl",
+        "image_url": "http://forkify-api.herokuapp.com/images/breakfast_yogurt_bowlc10d.jpg",
+        "publisher": "101 Cookbooks",
+        "source_url": "http://www.101cookbooks.com/archives/pomegranate-yogurt-bowl-recipe.html",
+        "cooking_time": 60,
+        "ingredients": "None  For each bowl:, None  A big dollop of greek yogurt, 2 tbsps fresh pomegranate juice, None  A drizzle of honey, None  A handful of puffed quinoa crisps, None  Sprinkling of toasted sunflower seeds, None  Optional: whole pomegranate seeds or fresh/dried rose petals a bit of bee pollen",
+        "cuisine_type": "Italian",
+        "cleaned_ingredients": "None  For each bowl:, None  A big dollop of greek yogurt, fresh pomegranate juice, None  A drizzle of honey, None  A handful of puffed quinoa crisps, None  Sprinkling of toasted sunflower seeds, None  Optional: whole pomegranate seeds or fresh/dried rose petals a bit of bee pollen"
+    },
+                               ]
 
         self.stdout.write("Finding similar recipes...\n")
         RecommendationSystem.recommend_similar_recipes(favorite_recipe)
