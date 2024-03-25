03df523e6600234b1c37e5e71fdd3f7ba4b0e349
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-03-26 02:15:35 +0700

    exclude the favourite recipe from being recommended

--- RecommendationSystem/management/commands/find_similar_recipe_2.py
+++ RecommendationSystem/management/commands/find_similar_recipe_2.py
@@ -19,7 +19,7 @@ class RecommendationSystem:
     2 TF-IDF Vectorization then cosine similarity.
     """
     @staticmethod
-    def recommend_similar_recipes(favorite_recipe):
+    def recommend_similar_recipes(favorite_recipes):
         recipes_queryset = Recipe.objects.all()
         recipes = [
             {
@@ -31,7 +31,7 @@ class RecommendationSystem:
         ]
         
         # Extracting favorite ingredients and ingredients for each recipe
-        favorite_ingredients = [ingredient.strip() for ingredient in favorite_recipe[0]['cleaned_ingredients'].split(',')]
+        favorite_ingredients = [ingredient.strip() for favorite_recipe in favorite_recipes for ingredient in favorite_recipe['cleaned_ingredients'].split(',')]
         recipe_ingredients = [recipe['cleaned_ingredients'] for recipe in recipes]
         
         # Create TF-IDF vectors
@@ -40,16 +40,23 @@ class RecommendationSystem:
         
         # Calculate cosine similarity between user's favorite ingredients and recipes
         similarity_scores = cosine_similarity(tfidf_matrix[-1], tfidf_matrix[:-1])
-
+        
         # Associate similarity scores with recipes
         recipe_similarity_scores = {recipes[i]['recipe_id']: similarity_scores[0][i] for i in range(len(recipes))}
+        
+        # Exclude favorite recipes from recommendations by setting their scores to 0
+        for favorite_recipe in favorite_recipes:
+            recipe_id = favorite_recipe['recipe_id']
+            if recipe_id in recipe_similarity_scores:
+                recipe_similarity_scores[recipe_id] = 0
+
         
         # Sort recipes based on similarity score
         sorted_recipes = sorted(recipes, key=lambda x: recipe_similarity_scores[x['recipe_id']], reverse=True)
 
         # Print recommended recipes
         print("Top 10 similar recipes:")
-        for i, recipe in enumerate(sorted_recipes[:10], 1):
+        for i, recipe in enumerate(sorted_recipes[:1000], 1):
             print(f"{i}. {recipe['title']} (similarity score: {recipe_similarity_scores[recipe['recipe_id']]:.4f})")
 
 class Command(BaseCommand):
@@ -61,6 +68,11 @@ class Command(BaseCommand):
                 "recipe_id": "5ed6604591c37cdc054bcfbd",
                 "title": "sweet potato (and marshmallow) biscuits",
                 "cleaned_ingredients": "pound sweet potatoes, buttermilk, all-purpose flour, baking powder, granulated sugar, ground cinnamon, ground nutmeg, ground ginger, ground cloves, table salt, unsalted butter cold, miniature marshmallows"
+            }
+            ,           {
+                "recipe_id": "5ed6604591c37cdc054bcec6",
+                "title": "Aegean Chicken Salad",
+                "cleaned_ingredients": "can chicken breast chunks drained, finely chopped red onion, fresh lemon juice, greek yogurt, hummus, kalamata olives chopped, finely chopped red bell pepper, chopped fresh dill, crumbled feta cheese"
             }
         ]
 
