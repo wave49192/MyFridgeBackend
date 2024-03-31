bfc0f37abbc93b7c7381e491bc533cda0bb515f9
Poomtum Rattanarat <poomtum2544@hotmail.com>
2024-03-31 21:39:42 +0700

    Update viewset method

--- Inventory/views.py
+++ Inventory/views.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
 
 from Inventory.models import Ingredient, Inventory
 from Inventory.serializers import IngredientSerializer, InventorySerializer
@@ -11,9 +12,19 @@ class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
     
+    def list(self, request):
+        queryset = Ingredient.objects.all()
+        serializer = IngredientSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
 class InventoryViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
+    
+    def list(self, request):
+        queryset = Inventory.objects.all()
+        serializer = InventorySerializer(queryset, many=True)
+        return Response(serializer.data)
